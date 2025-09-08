c3b700d7a53ff3c2ec533e135e7d71368f4deed6
rahmasahnouni <rahmasahnouni704@gmail.com>
2025-09-08 12:42:33 +0200

    mycommittt

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -7,7 +7,7 @@ namespace PartsUnlimited.Models
     {
         [Key]
         public int CartItemId { get; set; }
-// rahma
+// rahma kjrfke
         [Required]
         public string CartId { get; set; }
         public int ProductId { get; set; }
